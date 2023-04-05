ca23699f9ce9babf6b0f14e7611d85b9b10bbe1a
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-04-05 13:15:47 +0300

    5 lw

---
+++ beautysalon.sql
@@ -0,0 +1,64 @@
+CREATE DATABASE beauty_salon;
+
+USE beauty_salon;
+
+CREATE TABLE administrator (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  login VARCHAR(30) NOT NULL,
+  password VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE client (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  fullName VARCHAR(50) NOT NULL,
+  phone_number VARCHAR(20),
+  email VARCHAR(50),
+  birthday DATE
+);
+
+CREATE TABLE master (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  fullName VARCHAR(50) NOT NULL,
+  coefficient DECIMAL(6, 2) NOT NULL,
+  CHECK (coefficient>=1)
+);
+
+CREATE TABLE services (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  category VARCHAR(40) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  duration_minutes INT NOT NULL
+);
+
+CREATE TABLE master_services (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  master_id INT,
+  service_id INT,
+  FOREIGN KEY (master_id) REFERENCES master(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+CREATE TABLE client_master_services (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  client_id INT,
+  master_id INT,
+  service_id INT,
+  date_signup DATE NOT NULL,
+  time_signup TIME NOT NULL,
+  FOREIGN KEY (client_id) REFERENCES client(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  FOREIGN KEY (master_id) REFERENCES master(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+CREATE TABLE schedule (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  client_id INT,
+  master_id INT,
+  service_id INT,
+  start_time DATETIME NOT NULL,
+  end_time DATETIME NOT NULL,
+  FOREIGN KEY (client_id) REFERENCES client(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  FOREIGN KEY (master_id) REFERENCES master(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
